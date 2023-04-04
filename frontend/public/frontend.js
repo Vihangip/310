0240b949ee02fd3dcfda687bfd4af30a4558352b
Zoe Harris <zoeharr@students.cs.ubc.ca>
2023-04-04 15:11:08 -0700

    Make query object based on information in form

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,52 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+function parseFormData(formData) {
+	const stringFields = ["uuid", "id", "title", "instructor", "dept"]
+	const numberFields = ["year", "avg", "pass", "fail", "audit"];
+	const datasetName = "sections";
+	let basicQuery =
+		{
+			"WHERE": {},
+			"OPTIONS": {
+				"COLUMNS": []
+			}
+		};
+	let conditions = [];
+	let columns = [];
 
-function handleClickMe() {
-	alert("Button Clicked!");
+	for (let f of stringFields) {
+		if (formData.get(f) != "") {
+			let basicIS = {"IS": {}};
+			basicIS.IS[(datasetName + "_" + f)] = formData.get(f);
+			conditions.push(basicIS);
+		}
+		columns.push(datasetName + "_" + f);
+	}
+	for (let f of numberFields) {
+		if (formData.get(f) != "") {
+			let basicMComp = {};
+			let thing = {[(datasetName + "_" + f)]: parseInt(formData.get(f))};
+			console.log(thing);
+			basicMComp[formData.get(f + "-comparison")] = thing;
+			conditions.push(basicMComp);
+		}
+		columns.push(datasetName + "_" + f);
+	}
+	if (conditions.length > 0) {
+		basicQuery.WHERE = {
+			"AND": conditions
+		};
+	}
+	basicQuery.OPTIONS.COLUMNS = columns; // todo actually be able to select
+	return basicQuery;
 }
+
+function handleQuerySubmit(event) {
+	event.preventDefault();
+	const formData = new FormData(event.target);
+	const query = parseFormData(formData);
+	console.log(query);
+}
+
+document.getElementById("query-form").addEventListener('submit', handleQuerySubmit);
+
+// todo: send query (AJAX call)
+// todo: update page
