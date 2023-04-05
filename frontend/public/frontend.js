310def1d84f28756c77a595e0196659eadb44a18
Zoe Harris <zoeharr@students.cs.ubc.ca>
2023-04-05 12:52:26 -0700

    Load dataset when server starts, make AJAX query request

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -30,13 +30,33 @@ function parseFormData(formData) {
 		}
 		columns.push(datasetName + "_" + f);
 	}
-	if (conditions.length > 0) {
+	if (conditions.length > 1) {
 		basicQuery.WHERE = {
 			"AND": conditions
 		};
+	} else if (conditions.length === 1) {
+		basicQuery.WHERE = conditions[0];
 	}
 	basicQuery.OPTIONS.COLUMNS = columns; // todo actually be able to select
 	return basicQuery;
+}
+
+function handleHttpRequest() {
+	// this is where you deal with the response from the server, e.g. displaying results
+	// todo: display results
+
+	if (httpRequest.readyState === XMLHttpRequest.DONE) {
+		if (httpRequest.status === 200) {
+			// No problem with request, display here
+			console.log(httpRequest.responseText);
+		} else {
+			// There was a problem with the request.
+			// For example, the response may have a 404 (Not Found)
+			// or 500 (Internal Server Error) response code.
+		}
+	} else {
+		// Not ready yet.
+	}
 }
 
 function handleQuerySubmit(event) {
@@ -44,9 +64,14 @@ function handleQuerySubmit(event) {
 	const formData = new FormData(event.target);
 	const query = parseFormData(formData);
 	console.log(query);
+
+	// much of the AJAX code is based heavily on the provided AJAX documentation:
+	// https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started
+	httpRequest.open("POST", "http://localhost:4321/query", true);
+	httpRequest.setRequestHeader("Content-Type","application/json");
+	httpRequest.send(JSON.stringify(query));
 }
 
+let httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = handleHttpRequest;
 document.getElementById("query-form").addEventListener('submit', handleQuerySubmit);
-
-// todo: send query (AJAX call)
-// todo: update page
